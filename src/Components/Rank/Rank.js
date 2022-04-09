b6b65f8b174d4ab4dd4506090e650c493f937ca2
Emmbrik <hemmyemmix@gmail.com>
2022-04-09 11:45:37 +0100

    Added react-tsparticles

---
+++ src/Components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const Logo = () => {
+    return(
+       <div>
+           <div className="white f3">
+               {'Emmanuel your current rank is ...'}
+           </div>
+           <div className="white bold f1">
+               {'#5'}
+           </div>
+       </div>
+    );
+}
+
+export default Logo;
